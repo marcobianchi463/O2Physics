e07b59146cbfbb31f8eeac2615eb7cb2221fea77
Marco Bianchi <marco.bianchi463@edu.unito.it>
2025-06-11 13:38:19 +0200

    Fix O2 linter

--- PWGLF/TableProducer/Nuspex/nucleiFlowTree.cxx
+++ PWGLF/TableProducer/Nuspex/nucleiFlowTree.cxx
@@ -19,6 +19,10 @@
 // o2-analysis-pid-tof-base, o2-analysis-multiplicity-table, o2-analysis-event-selection
 // (to add flow: o2-analysis-qvector-table, o2-analysis-centrality-table)
 
+/// \file nucleiFlowTree.cxx
+/// \author Marco Bianchi (mbianchi@cern.ch)
+/// \brief Task to produce nuclei flow tree
+
 #include <algorithm>
 #include <cmath>
 #include <memory>
@@ -307,7 +311,7 @@ struct nucleiFlowTree {
       {nuclei::charges[3] * cfgMomentumScalingBetheBloch->get(3u, 0u) / nuclei::masses[3], nuclei::charges[3] * cfgMomentumScalingBetheBloch->get(3u, 1u) / nuclei::masses[3]},
       {nuclei::charges[4] * cfgMomentumScalingBetheBloch->get(3u, 0u) / nuclei::masses[4], nuclei::charges[4] * cfgMomentumScalingBetheBloch->get(3u, 1u) / nuclei::masses[4]}};
 
-    for (auto& track : tracks) { // start loop over tracks
+    for (auto& const track : tracks) { // start loop over tracks
       if (std::abs(track.eta()) > cfgCutEta ||
           track.tpcInnerParam() < cfgCutTpcMom ||
           track.itsNCls() < cfgCutNclusITS ||
@@ -455,10 +459,10 @@ struct nucleiFlowTree {
       return;
     }
     fillDataInfo(collision, tracks);
-    for (auto& c : nuclei::candidates) {
+    for (auto& const c : nuclei::candidates) {
       nucleiTable(c.pt, c.eta, c.phi, c.tpcInnerParam, c.beta, c.zVertex, c.DCAxy, c.DCAz, c.TPCsignal, c.ITSchi2, c.TPCchi2, c.TOFchi2, c.flags, c.TPCfindableCls, c.TPCcrossedRows, c.ITSclsMap, c.TPCnCls, c.TPCnClsShared, c.clusterSizesITS);
     }
-    for (auto& c : nuclei::candidates_flow) {
+    for (auto& const c : nuclei::candidates_flow) {
       nucleiTableFlow(c.centFV0A, c.centFT0M, c.centFT0A, c.centFT0C, c.psiFT0A, c.psiFT0C, c.psiTPC, c.psiTPCl, c.psiTPCr, c.qFT0A, c.qFT0C, c.qTPC, c.qTPCl, c.qTPCr);
     }
   }
@@ -472,10 +476,10 @@ struct nucleiFlowTree {
       return;
     }
     fillDataInfo(collision, tracks);
-    for (auto& c : nuclei::candidates) {
+    for (auto& const c : nuclei::candidates) {
       nucleiTable(c.pt, c.eta, c.phi, c.tpcInnerParam, c.beta, c.zVertex, c.DCAxy, c.DCAz, c.TPCsignal, c.ITSchi2, c.TPCchi2, c.TOFchi2, c.flags, c.TPCfindableCls, c.TPCcrossedRows, c.ITSclsMap, c.TPCnCls, c.TPCnClsShared, c.clusterSizesITS);
     }
-    for (auto& c : nuclei::candidates_flow) {
+    for (auto& const c : nuclei::candidates_flow) {
       nucleiTableFlow(c.centFV0A, c.centFT0M, c.centFT0A, c.centFT0C, c.psiFT0A, c.psiFT0C, c.psiTPC, c.psiTPCl, c.psiTPCr, c.qFT0A, c.qFT0C, c.qTPC, c.qTPCl, c.qTPCr);
     }
   }
